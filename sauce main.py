e8df86621766adfca60e8e879f56c573b9d6637c
hunkam <hudoka.dev@gmail.com>
2022-03-29 21:45:58 -0400

    hello everyone

---
+++ sauce main.py
@@ -0,0 +1,10 @@
+
+"""this is a reddit bot created for counting the amount of times a user request the SAUCE
+this will be created by using python, mysql and the reddit api wrapper praw
+this project will hopefully have tbe features of counting the amount of times the user asks for the SAUCE as well as
+being able to count it """
+
+import praw
+import random
+import json
+
